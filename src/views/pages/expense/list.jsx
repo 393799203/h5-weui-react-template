9ad507a6c2c165989ef5f5093b4c033f2fde6518
qingmeng <qingmeng@mogujie.com>
2016-10-25 18:16:36 +0800

    qm

--- src/views/pages/expense/list.jsx
+++ src/views/pages/expense/list.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import { List } from 'tui';
+import { Tabs, List } from 'tui';
 const ListItem = List.Item;
+const TabPane = Tabs.TabPane;
 
-export default class Test extends Component {
+export default class ExpenseList extends Component {
 	render() {
 		return (
 			<div>
+				<Tabs defaultActiveKey="1">
+				    <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+				    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+				    <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+				</Tabs>
 				<List header="头部" footer="底部">
 					<ListItem key="2" arrow hover>
 						<div>列表标题1</div>

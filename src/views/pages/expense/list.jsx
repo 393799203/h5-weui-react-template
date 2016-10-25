4cdebc73b1a0292f3d1ff7b6e3d00a9181330c5a
qingmeng <qingmeng@mogujie.com>
2016-10-25 15:33:10 +0800

    qm

--- src/views/pages/expense/list.jsx
+++ src/views/pages/expense/list.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Tab from 'components/tab';
+import List from 'components/list';
 
 export default class Test extends Component {
 	render() {
 		return (
 			<div>
-				list
+				<Tab />
+				<List />
 			</div>
 		)
 	}

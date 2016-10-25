735998180f2ad2fdb463b6388371cccbc74306f3
qingmeng <qingmeng@mogujie.com>
2016-10-25 16:07:54 +0800

    qm

--- src/views/pages/expense/list.jsx
+++ src/views/pages/expense/list.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-import Tab from 'components/tab';
-import List from 'components/list';
+import { List } from 'tui';
+const ListItem = List.Item;
 
 export default class Test extends Component {
 	render() {
 		return (
 			<div>
-				<Tab />
-				<List />
+				<List header="头部" footer="底部">
+					<ListItem key="2" arrow hover>
+						<div>列表标题1</div>
+						<div className="text-lighter">
+							测试
+						</div>
+					</ListItem>
+					<ListItem key="3" arrow hover>
+						<div>列表标题2</div>
+						<div className="text-lighter">
+							测试
+						</div>
+					</ListItem>
+				</List>
 			</div>
 		)
 	}

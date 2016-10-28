1510ef309a362d39c50e365c37fd15552886789e
qingmeng <qingmeng@mogujie.com>
2016-10-28 13:51:53 +0800

    qm

--- src/views/pages/expense/detail.jsx
+++ src/views/pages/expense/detail.jsx
@@ -5,19 +5,24 @@ import Util from 'core/util';
 export default class ExpenseDetail extends Component {
 	
 	state = {
-		detailInfo: {}
+		detailInfo: {
+			detailFormList: []
+		}
 	}
 
 	constructor(props){
 		super(props);
-		Util.startLoading();
-		Ajax.get("/expense/request/detail", {id: props.params.id, updated: props.location.query.updated}).then((res)=>{
+	}
+
+	componentDidMount() {
+	 	Util.startLoading();
+		Ajax.get("/expense/request/detail", {id: this.props.params.id, updated: this.props.location.query.updated}).then((res)=>{
 			this.state.detailInfo = res.data;
 			this.setState(this.state);
 			Util.closeLoading();
 		}, (err) => {
 			Util.closeLoading();
-		});
+		});     
 	}
 
 	render() {
@@ -58,18 +63,62 @@ export default class ExpenseDetail extends Component {
 		            </div>
 		            <div className="weui-cell bg-white">
 		                <div className="weui-cell__bd">
-		                    <p>报销说明</p>
-		                </div>
-		                <div className="weui-cell__ft">{detailInfo.content}</div>
-		            </div>
-		            <div className="weui-cell bg-white">
-		                <div className="weui-cell__bd">
 		                    <p>总计金额</p>
 		                </div>
 		                <div className="weui-cell__ft">{Util.money(detailInfo.amt || 0)}</div>
 		            </div>
+		            <div className="weui-cell bg-white">
+			            <If condition={ detailInfo.content && detailInfo.content.length > 10 }>
+			            	<div className="weui-media-box_text">
+			                    <h4 className="weui-media-box__title">报销说明</h4>
+			                    <p className="weui-media-box__desc">{detailInfo.content}</p>
+			                </div>
+			            <Else />
+			                <div className="weui-cell__bd">
+			                    <p>报销说明</p>
+			                </div>
+			                <div className="weui-cell__ft">{detailInfo.content}</div>
+			            </If>
+		            </div>
 		        </div>
 		        <div className="weui-cells__title">其他明细信息</div>
+		        <div className="weui-cells m-t-n">
+		        	<For each = "item" of = { detailInfo.detailFormList } index = "index">
+		        	<div className="bg-white m-b" key={ index }>
+		        		<div className="weui-cell">
+			                <div className="weui-cell__bd">
+			                    <p>时间</p>
+			                </div>
+			                <div className="weui-cell__ft">{item.actionDate}</div>
+			            </div>
+			            <div className="weui-cell">
+			                <div className="weui-cell__bd">
+			                    <p>费用类型</p>
+			                </div>
+			                <div className="weui-cell__ft">{item.expenseTypeName}</div>
+			            </div>
+			            <div className="weui-cell">
+			                <div className="weui-cell__bd">
+			                    <p>金额</p>
+			                </div>
+			                <div className="weui-cell__ft">{Util.money(item.amt)}</div>
+			            </div>
+			            <div className="weui-cell">
+			            	<If condition={ item.comments && item.comments.length > 10 }>
+				            	<div className="weui-media-box_text">
+				                    <h4 className="weui-media-box__title">说明</h4>
+				                    <p className="weui-media-box__desc">{item.comments}</p>
+				                </div>
+				            <Else />
+				                <div className="weui-cell__bd">
+				                    <p>明细说明</p>
+				                </div>
+				                <div className="weui-cell__ft">{item.comments}</div>
+				            </If>
+			            </div>
+		        	</div>
+		            </For>
+		        </div>
 			</div>
 		)
 	}

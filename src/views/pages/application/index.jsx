1068d559c1f07932f3cf5c69520e7d5652e69b20
qingmeng <qingmeng@mogujie.com>
2016-11-04 16:20:02 +0800

    qm

--- src/views/pages/application/index.jsx
+++ src/views/pages/application/index.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from  'react-router'
+import List from 'components/list';
+import Ajax from 'core/ajax';
+import Util from 'core/util';
 
 export default class Application extends Component {
+	state = {
+		params: {
+			"outlineType": 1
+		},
+		ajaxUrl: "/expense/request/getMyOutlineList"
+	}
+
+	constructor(props){
+		super(props);
+		Util.setTitle("我的报销");
+	}
+
 	render() {
+		let params = this.state.params;
+		let ajaxUrl = this.state.ajaxUrl;
 		return (
-			<div>
-				该页面怎么设计呢~
-			</div>
+			<List className="m-t-n" params = { params } ajaxUrl = { ajaxUrl }/>
 		)
 	}
 }

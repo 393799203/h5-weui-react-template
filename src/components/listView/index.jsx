7e0e7d4a445c45eeb4b28ef52b3c745aa5577eee
qingmeng <qingmeng@mogujie.com>
2016-11-09 23:08:15 +0800

    q,

--- src/components/listView/index.jsx
+++ src/components/listView/index.jsx
@@ -52,7 +52,7 @@ export default class ListView extends Component {
             // 支持鼠标事件，因为我开发是PC鼠标模拟的
             mouseWheel: true,
             // 滚动事件的探测灵敏度，1-3，越高越灵敏，兼容性越好，性能越差
-            probeType: 2,
+            probeType: 3,
             // 拖拽超过上下界后出现弹射动画效果，用于实现下拉/上拉刷新
             bounce: true,
             // 展示滚动条

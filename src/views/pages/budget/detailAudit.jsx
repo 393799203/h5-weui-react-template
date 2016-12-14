cf636b7745ff9b6dc7cede7223a70ae890a7466c
qingmeng <qingmeng@mogujie.com>
2016-12-14 15:27:37 +0800

    qm

--- src/views/pages/budget/detailAudit.jsx
+++ src/views/pages/budget/detailAudit.jsx
@@ -7,6 +7,7 @@ import Util from 'core/util';
 export default class ExpenseDetail extends BaseComponent {
 	
 	state = {
+		currentYear: parseInt(moment().format('YYYY')),
 		detailInfo: {},
 		showAudit: false,
 		disabled: false
@@ -60,6 +61,7 @@ export default class ExpenseDetail extends BaseComponent {
 
 	render() {
 		let detailInfo = this.state.detailInfo;
+		let currentYear = this.state.currentYear;
 		let showAudit = this.state.showAudit;
 		let disabled = this.state.disabled;
 		return (
@@ -90,57 +92,160 @@ export default class ExpenseDetail extends BaseComponent {
 		                </div>
 		                <div className="weui-cell__ft">{detailInfo.applyDate}</div>
 		            </div>
+		            <div className="weui-cell">
+		                <div className="weui-cell__bd">
+		                    <p>申请类型</p>
+		                </div>
+		                <div className="weui-cell__ft">{detailInfo.isChangedDesc}</div>
+		            </div>
 		        </div>
-	        	<div className="weui-cells__title">详细预算信息</div>
-	        	<div className="weui-cells m-t-n">
-		        	<For each = "item" of = { detailInfo.budgetRequestItemFormList || [] } index = "index">
-			        	<div className={classnames("bg-white", {"m-b": index != detailInfo.budgetRequestItemFormList.length -1})} key={ index }>
-			        		<div className="weui-cell">
-				                <div className="weui-cell__bd">
-				                    <div className="pull-left">预算类目</div>
-				                    <div className="pull-right m-r-sm text-light">{item.budgetCategoryName}</div>
-				                </div>
-				                <div className="weui-cell__bd">
-				                    <div className="pull-left m-l-sm">费用合计</div>
-				                    <div className="pull-right text-light">￥{Util.money(item.q1Sum + item.q2Sum + item.q3Sum + item.q4Sum)}</div>
-				                </div>
-				            </div>
-				            <div className="weui-cell">
-				            	<div className="weui-cell__bd">
-				                    <div className="pull-left">Q1</div>
-				                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q1Sum)}</div>
-				                </div>
-				                <div className="weui-cell__bd">
-				                    <div className="pull-left m-l-sm">Q2</div>
-				                    <div className="pull-right text-light">￥{Util.money(item.q2Sum)}</div>
-				                </div>
-				            </div>
-				            <div className="weui-cell">
-				            	<div className="weui-cell__bd">
-				                    <div className="pull-left">Q3</div>
-				                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q3Sum)}</div>
-				                </div>
-				                <div className="weui-cell__bd">
-				                    <div className="pull-left m-l-sm">Q4</div>
-				                    <div className="pull-right text-light">￥{Util.money(item.q4Sum)}</div>
-				                </div>
-				            </div>
-				            <div className="weui-cell">
-				            	<If condition={ item.comments && item.comments.length > 15 }>
-					            	<div className="weui-media-box_text">
-					                    <h4 className="weui-media-box__title">备注</h4>
-					                    <p className="weui-media-box__desc text-normal">{item.remark}</p>
-					                </div>
-					            <Else />
+		        <If condition= { !detailInfo.isChanged } >
+		        	<div className="weui-cells__title">预算编制信息</div>
+		        	<div className="weui-cells m-t-n">
+			        	<For each = "item" of = { detailInfo.budgetRequestItemFormList || [] } index = "index">
+				        	<div className={classnames("bg-white", {"m-b": index != detailInfo.budgetRequestItemFormList.length -1})} key={ index }>
+				        		<div className="weui-cell">
 					                <div className="weui-cell__bd">
-					                    <p>备注</p>
+					                    <div className="pull-left">预算类目</div>
+					                    <div className="pull-right m-r-sm text-light">{item.budgetCategoryName}</div>
 					                </div>
-					                <div className="weui-cell__ft">{item.remark}</div>
-					            </If>
-				            </div>
-			        	</div>
-		            </For>
-		        </div>
+					                <div className="weui-cell__bd">
+					                    <div className="pull-left m-l-sm">费用合计</div>
+					                    <div className="pull-right text-light">￥{Util.money(item.q1Sum + item.q2Sum + item.q3Sum + item.q4Sum)}</div>
+					                </div>
+					            </div>
+					            <div className="weui-cell">
+					            	<div className="weui-cell__bd">
+					                    <div className="pull-left">Q1</div>
+					                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q1Sum)}</div>
+					                </div>
+					                <div className="weui-cell__bd">
+					                    <div className="pull-left m-l-sm">Q2</div>
+					                    <div className="pull-right text-light">￥{Util.money(item.q2Sum)}</div>
+					                </div>
+					            </div>
+					            <div className="weui-cell">
+					            	<div className="weui-cell__bd">
+					                    <div className="pull-left">Q3</div>
+					                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q3Sum)}</div>
+					                </div>
+					                <div className="weui-cell__bd">
+					                    <div className="pull-left m-l-sm">Q4</div>
+					                    <div className="pull-right text-light">￥{Util.money(item.q4Sum)}</div>
+					                </div>
+					            </div>
+					            <div className="weui-cell">
+					            	<If condition={ item.comments && item.comments.length > 15 }>
+						            	<div className="weui-media-box_text">
+						                    <h4 className="weui-media-box__title">备注</h4>
+						                    <p className="weui-media-box__desc text-normal">{item.remark}</p>
+						                </div>
+						            <Else />
+						                <div className="weui-cell__bd">
+						                    <p>备注</p>
+						                </div>
+						                <div className="weui-cell__ft">{item.remark}</div>
+						            </If>
+					            </div>
+				        	</div>
+			            </For>
+			        </div>
+		        <Else />
+		        	<div className="weui-cells__title">预算调整信息</div>
+		        	<div className="weui-cells m-t-n">
+			        	<For each = "item" of = { detailInfo.budgetRequestItemShipDtoList || [] } index = "index">
+				        	<div className={classnames("bg-white", {"m-b": index != detailInfo.budgetRequestItemShipDtoList.length -1})} key={ index }>
+				        		<div className="weui-cell">
+					                <div className="weui-cell__bd">
+					                    <div className="pull-left">预算类目</div>
+					                    <div className="pull-right text-light">{item.budgetCategoryName}</div>
+					                </div>
+					            </div>
+					            <div className="weui-cell">
+					            	<If condition={ detailInfo.budgetYear > currentYear ||(detailInfo.budgetYear == currentYear && moment().quarter() <= 1) }>
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q1调整前</div>
+						                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q1Sum)}</div>
+						                </div>
+						                <div className="weui-cell__bd">
+						                    <div className="pull-left m-l-sm">调整后</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.changedQ1Sum)}</div>
+						                </div>
+						            <Else />
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q1实际值</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.actualQ1Sum)}</div>
+						                </div>
+						            </If>
+					            </div>
+					            <div className="weui-cell">
+					            	<If condition={ detailInfo.budgetYear > currentYear || ( detailInfo.budgetYear == currentYear && moment().quarter() <= 2) }>
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q2调整前</div>
+						                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q2Sum)}</div>
+						                </div>
+						                <div className="weui-cell__bd">
+						                    <div className="pull-left m-l-sm">调整后</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.changedQ2Sum)}</div>
+						                </div>
+					                <Else />
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q2实际值</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.actualQ2Sum)}</div>
+						                </div>
+						            </If>
+					            </div>
+					            <div className="weui-cell">
+					            	<If condition={ detailInfo.budgetYear > currentYear || ( detailInfo.budgetYear == currentYear && moment().quarter() <= 3) }>
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q3调整前</div>
+						                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q3Sum)}</div>
+						                </div>
+						                <div className="weui-cell__bd">
+						                    <div className="pull-left m-l-sm">调整后</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.changedQ3Sum)}</div>
+						                </div>
+						            <Else />
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q3实际值</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.actualQ3Sum)}</div>
+						                </div>
+						            </If>
+					            </div>
+					            <div className="weui-cell">
+					            	<If condition={ detailInfo.budgetYear > currentYear || ( detailInfo.budgetYear == currentYear && moment().quarter() <= 4) }>
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q4调整前</div>
+						                    <div className="pull-right m-r-sm text-light">￥{Util.money(item.q4Sum)}</div>
+						                </div>
+						                <div className="weui-cell__bd">
+						                    <div className="pull-left m-l-sm">调整后</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.changedQ4Sum)}</div>
+						                </div>
+						            <Else />
+						            	<div className="weui-cell__bd">
+						                    <div className="pull-left">Q4实际值</div>
+						                    <div className="pull-right text-light">￥{Util.money(item.actualQ4Sum)}</div>
+						                </div>
+						            </If>
+					            </div>
+					            <div className="weui-cell">
+					            	<If condition={ item.comments && item.comments.length > 15 }>
+						            	<div className="weui-media-box_text">
+						                    <h4 className="weui-media-box__title">备注</h4>
+						                    <p className="weui-media-box__desc text-normal">{item.remark}</p>
+						                </div>
+						            <Else />
+						                <div className="weui-cell__bd">
+						                    <p>备注</p>
+						                </div>
+						                <div className="weui-cell__ft">{item.remark}</div>
+						            </If>
+					            </div>
+				        	</div>
+			            </For>
+			        </div>
+		        </If>
 		        <If condition = {showAudit}>
 			        <div className="auditArea">
 			        	<div className="weui-cells weui-cells_form">

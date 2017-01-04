b2e9f810596f956651f66ad99945ea782a57f483
qingmeng <qingmeng@mogujie.com>
2017-01-04 15:45:31 +0800

    qm

--- src/views/pages/travel/apply.jsx
+++ src/views/pages/travel/apply.jsx
@@ -1,15 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import BaseComponent from 'core/baseComponent';
+import Ajax from 'core/ajax';
+import Util from 'core/util';
+import Global from 'server/global';
 
-export default class Test extends Component {
+export default class TravelApply extends BaseComponent {
 	state = {
 		deptList: [],
 		params: {
-			applyName: "",
+			nickName: "",
 			outlineType: "5",
 			balanceDate: "",
 			regionCode: "",
 			companyId: ""
 		}
+	}
+
+	componentDidMount() {
+		let deptListPromise = Global.getDept();
+		let getCurrentUserPromise = Global.getCurrentUser();
+		Promise.all([deptListPromise, getCurrentUserPromise]).then(res => {
+			this.state.deptList = res[0].data.list || [];
+			this.state.params.nickName = res[1].data.nickName;
+			this.state.params.deptId = res[1].data.biz1thDeptId;
+			this.setState(this.state);
+		}, (err) => {
+			setTimeout(()=>{
+				Util.popWindow("/apply");
+			}, 2000);
+		})
 	}
 
 	render() {
@@ -23,7 +42,7 @@ export default class Test extends Component {
 		                    <label htmlFor="date" className="weui-label">申请人</label>
 		                </div>
 		                <div className="weui-cell__bd">
-		                    <input className="weui-input" type="string" value={ params.applyName } onChange={(e) => { params.applyName = e.target.value; this.setState(this.state);}}/>
+		                    <input className="weui-input" type="string" value={ params.nickName } onChange={(e) => { params.nickName = e.target.value; this.setState(this.state);}}/>
 		                </div>
 		            </div>
 		            <div className="weui-cell weui-cell_select weui-cell_select-after bg-white">
@@ -31,7 +50,7 @@ export default class Test extends Component {
 		                    <label htmlFor="dept" className="weui-label">部门</label>
 		                </div>
 		                <div className="weui-cell__bd">
-		                    <select className="weui-select" name="dept" value={params.deptId} onChange={ (e) => { params.deptId = e.target.value; this.getDetailList() }}>
+		                    <select className="weui-select" name="dept" value={params.deptId} onChange={ (e) => { params.deptId = e.target.value; this.setState(this.state);}}>
 		                    	<For each = "item" of = { deptList } index = "index">
 		                    		<option value={ item.deptId } key={index}>{ item.deptName }</option>
 		                    	</For>
@@ -39,6 +58,7 @@ export default class Test extends Component {
 		                </div>
 		            </div>
 		        </div>
+		        <div className="weui-cells__title">出行信息</div>
 			</div>
 		)
 	}

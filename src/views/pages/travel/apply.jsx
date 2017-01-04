34b06647e0716e8056fe221c124059f927b4900a
qingmeng <qingmeng@mogujie.com>
2017-01-04 17:22:05 +0800

    mq

--- src/views/pages/travel/apply.jsx
+++ src/views/pages/travel/apply.jsx
@@ -7,7 +7,8 @@ import Global from 'server/global';
 export default class TravelApply extends BaseComponent {
 	state = {
 		params: {
-			nickName: ""
+			nickName: "",
+			deptName: ""
 		}
 	}
 
@@ -32,22 +33,25 @@ export default class TravelApply extends BaseComponent {
 				<div className="weui-cells m-t-n">
 					<div className="weui-cell bg-white">
 		                <div className="weui-cell__hd">
-		                    <label htmlFor="date" className="weui-label">申请人</label>
+		                    <label htmlFor="nickName" className="weui-label">申请人</label>
 		                </div>
 		                <div className="weui-cell__bd">
-		                    <input className="weui-input" type="text" value={ params.nickName } disabled/>
+		                    <input className="weui-input" type="text" name="nickName" value={ params.nickName } disabled/>
 		                </div>
 		            </div>
 		            <div className="weui-cell bg-white">
 		                <div className="weui-cell__hd">
-		                    <label htmlFor="date" className="weui-label">部门</label>
+		                    <label htmlFor="dept" className="weui-label">部门</label>
 		                </div>
 		                <div className="weui-cell__bd">
-		                    <input className="weui-input" type="text" value={ params.deptName } disabled/>
+		                    <input className="weui-input" type="text" name="dept" value={ params.deptName } disabled/>
 		                </div>
 		            </div>
 		        </div>
 		        <div className="weui-cells__title">出行信息</div>
+		        <div className="weui-cells m-t-n">
+		        	
+		        </div>
 			</div>
 		)
 	}

6e08382f122d419b476e1b95fc1ce36c8df6be82
qingmeng <qingmeng@mogujie.com>
2017-01-16 20:47:10 +0800

    qm

--- src/views/pages/gotoCtrip.jsx
+++ src/views/pages/gotoCtrip.jsx
@@ -24,7 +24,8 @@ export default class GoToCtrip extends BaseComponent {
 		let token = this.state.token;
 		return (
 			<form style={{"display":"none"}} action="https://ct.ctrip.com/m/SingleSignOn/H5SignInfo" id="H5SignInfo" method="post"> 
-                <input type="hidden" name="AppKey" value={token.AppKey}/> 
+                <input type="hidden" name="AppKey" value={token.AppKey}/>
+                <input type="hidden" name="AppSecurity" value={token.AppSecurity}/> 
                 <input type="hidden"  name="Ticket" value={token.Ticket}/> 
                 <input type="hidden" name="EmployeeID" value={token.EmployeeID}/>  
                 <input type="submit" value="机票预订" id="button"/> 

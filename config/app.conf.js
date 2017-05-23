4b2ea30007eb0a27896947b09f34d02a7cbfef71
393799203 <393799203@qq.com>
2017-05-23 17:07:43 +0800

    Update app.conf.js

--- config/app.conf.js
+++ config/app.conf.js
@@ -1,6 +1,6 @@
 var appConf = {
 	"port": 8888,
-	"serverName":"h5.travel.meili-inc.com",
-	"proxy": "http://test.trip.meili-inc.com"
+	"serverName":"h5.travel.xxxxx.com",
+	"proxy": "http://test.trip.xxxxx.com"
 }
 module.exports = appConf;
